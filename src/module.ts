46ff8b2ddec50627814144433ea2a3d53a74dcd1
Ari Chivukula <ari@chivuku.la>
2018-04-24 11:47:36 -0400

    File hash

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,17 @@
+import { createHash, Hash } from "crypto";
+
 import { IRenderable } from "./internal";
 
 export class TSModule implements IRenderable {
 
+  private static getHash(content: string): string {
+    const hash: Hash = createHash("SHA512");
+    hash.update(content);
+
+    return hash.digest("base64");
+  }
+
   public render(): string {
-    return "";
+    return TSModule.getHash("");
   }
 }
