306c92e0f960ba98563c7adad147fb5fb24a62d2
Ari Chivukula <ari@dds.mil>
2018-04-25 12:22:27 -0400

    Fix lint

--- src/__tests__/variable.test.ts
+++ src/__tests__/variable.test.ts
@@ -3,20 +3,23 @@ import { exportedVariable, immutableVariable, mutableVariable } from "./setup";
 test(
   "exportedVariable",
   async (): Promise<void> => {
-    expect(exportedVariable.render()).toMatchSnapshot();
+    expect(exportedVariable.render())
+      .toMatchSnapshot();
   },
 );
 
 test(
   "immutableVariable",
   async (): Promise<void> => {
-    expect(immutableVariable.render()).toMatchSnapshot();
+    expect(immutableVariable.render())
+      .toMatchSnapshot();
   },
 );
 
 test(
   "mutableVariable",
   async (): Promise<void> => {
-    expect(mutableVariable.render()).toMatchSnapshot();
+    expect(mutableVariable.render())
+      .toMatchSnapshot();
   },
 );
--- src/cli.ts
+++ src/cli.ts
@@ -1,46 +1,47 @@
 #!/usr/bin/env node
 
-import { Glob } from "glob";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
-import { dirname, sep } from "path"
+import { Glob } from "glob";
+import { dirname, sep } from "path";
 
 import { Module } from "./index";
 
-if (process.argv.length !== 3) {
+const expectedArgLength: number = 3;
+if (process.argv.length !== expectedArgLength) {
   throw new Error("Usage is `typescriptase GLOB`");
 }
-const moduleGlob: string = process.argv[2];
-
-
+const moduleGlob: string = process.argv[expectedArgLength - 1];
 
 new Glob(
   moduleGlob,
   {},
-  (err: Error | null, files: string[]) => {
-    if (err) {
+  (err: Error | null, files: string[]): void => {
+    if (err !== null) {
       throw err;
     }
     files.forEach(
       (modulePath: string) => {
         // tslint:disable-next-line
-        const codegen: any = require(`../${modulePath}`);
-        for (let key in codegen) {
+        const codegen: object = require(`../${modulePath}`);
+        for (const key in codegen) {
           if (codegen[key] instanceof Module) {
+            // tslint:disable-next-line
             const module: Module = codegen[key];
             const dir: string = dirname(module.destination());
             let partialDir: string = "";
-            dir.split(sep).forEach(
-              (currentValue: string, index: number) => {
-                partialDir += `${currentValue}${sep}`;
-                if (!existsSync(partialDir)) {
-                  mkdirSync(partialDir);
-                }
-              },
-            );
+            dir.split(sep)
+              .forEach(
+                (currentValue: string, index: number) => {
+                  partialDir += `${currentValue}${sep}`;
+                  if (!existsSync(partialDir)) {
+                    mkdirSync(partialDir);
+                  }
+                },
+              );
             writeFileSync(module.destination(), module.print(modulePath));
           }
         }
       },
     );
   },
-)
+);
