8e7c483c3fd36b5b34e50a79d111a8a9847a51e8
Ari Chivukula <ari@dds.mil>
2018-04-25 12:14:26 -0400

    WE DID IT!

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { Glob } from "glob";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { dirname, sep } from "path"
 
@@ -8,23 +9,38 @@ import { Module } from "./index";
 if (process.argv.length !== 3) {
   throw new Error("Usage is `typescriptase GLOB`");
 }
-const modulePath: string = process.argv[2];
+const moduleGlob: string = process.argv[2];
 
-// tslint:disable-next-line
-const codegen: any = require(modulePath);
-for (let key in codegen) {
-  if (codegen[key] instanceof Module) {
-    const module: Module = codegen[key];
-    const dir: string = dirname(module.destination());
-    let partialDir: string = "";
-    dir.split(sep).forEach(
-      (currentValue: string, index: number) => {
-        partialDir += `${currentValue}${sep}`;
-        if (!existsSync(partialDir)) {
-          mkdirSync(partialDir);
+
+
+new Glob(
+  moduleGlob,
+  {},
+  (err: Error | null, files: string[]) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach(
+      (modulePath: string) => {
+        // tslint:disable-next-line
+        const codegen: any = require(`../${modulePath}`);
+        for (let key in codegen) {
+          if (codegen[key] instanceof Module) {
+            const module: Module = codegen[key];
+            const dir: string = dirname(module.destination());
+            let partialDir: string = "";
+            dir.split(sep).forEach(
+              (currentValue: string, index: number) => {
+                partialDir += `${currentValue}${sep}`;
+                if (!existsSync(partialDir)) {
+                  mkdirSync(partialDir);
+                }
+              },
+            );
+            writeFileSync(module.destination(), module.print(modulePath));
+          }
         }
       },
     );
-    writeFileSync(module.destination(), module.print(modulePath));
-  }
-}
+  },
+)
